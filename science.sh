0afe27e43bee12c7ba7236b27f0174db2edfa808
iycdn <109452664+iycdn@users.noreply.github.com>
2022-07-19 12:31:14 +0800

    Update science.sh

--- science.sh
+++ science.sh
@@ -2,11 +2,10 @@
 
 # configs
 AUUID=9831667d-e2fc-4022-a7ab-5fb9e0f1ee71
-CADDYIndexPage=https://raw.githubusercontent.com/gitiy1/science/master/mikutap-master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/gitiy1/science/master/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/gitiy1/science/master/xray.json
+CADDYIndexPage=https://raw.githubusercontent.com/iycdn/science/master/mikutap-master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/iycdn/science/master/Caddyfile
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/gitiy1/science/master/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/iycdn/science/master/StoreFiles
 #PORT=4433
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
@@ -15,19 +14,10 @@ echo -e "BBR启动成功！"
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
-
-# storefiles
-mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
-wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
-
-for file in $(ls /usr/share/caddy/$AUUID); do
-    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
-done
 
 # start
 tor &
 
-/xray -config /xray.json &
+/naive --listen=socks://127.0.0.1:8080 &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
